03b8a79c9547ebc725604c2d0d76a9a0f319521a
Yuvaraj Madineni <yuvarajuv49@gmail.com>
2022-07-22 16:12:20 +0530

    add api and productlist component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
-import { Card } from "@mui/material";
 import type { NextPage } from "next";
-import React from "react";
-import card from "../components/card";
+import React, { useState } from "react";
+import ProductList from "../components/productlist";
 import SearchBar from "../components/SearchBar";
+import { ProductType } from "../provider/api";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [products, setProducts] = useState([] as ProductType[]);
   return (
     <div className={styles.container}>
-      <SearchBar />
-      <Card variant="outlined">{card}</Card>
+      <SearchBar setProudcts={setProducts} />
+      <ProductList products={products} />
     </div>
   );
 };
---
+++ provider/api.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export interface ProductType {
+  name: string;
+  fertiliser_group_name: string;
+  description: string;
+  Manufacturer_name?: string;
+  Country_of_origin?: string;
+  ProductVariants: ProductVariant[];
+  created_at?: string;
+  updated_at?: string;
+  productCategoryId?: number;
+  categoryName?: string;
+  Crops: Crop[];
+}
+interface Crop {
+  cropName: string;
+}
+interface ProductVariant {
+  id: number;
+  product_id?: number;
+  min_price: number;
+  max_price: number;
+  sellingPrice?: number;
+  sku: string;
+  size: string;
+  packaging: string;
+  photo: string;
+  internal_id: string;
+  quantity: number;
+  active?: boolean;
+}
+const fetchProducts = async (
+  search: string
+): Promise<ProductType[] | undefined> => {
+  const res = await axios.get(
+    `http://localhost:8000/api/v1/products?search=${search}`
+  );
+
+  return res.data.data.products;
+};
+
+export default fetchProducts;
